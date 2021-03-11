a029c5abcd8a56adbc1a1a01125331f8832a8022
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-03-11 19:28:17 +0100

    parallel validating

--- 01-dns-netcore/Solution.cs
+++ 01-dns-netcore/Solution.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Linq;
 
 namespace dns_netcore
@@ -18,11 +19,22 @@ namespace dns_netcore
 			this.dnsClient = client;
 			this.cache = new ConcurrentDictionary<string, IP4Addr>();
 		}
+		// Given a domain (mff.cuni.cz), creates a list of all subdomains, e.g:
+		// [cz, cuni.cz, mff.cuni.cz]
+		private List<string> generateSubdomains(string domain) {
+			var domains = domain.Split(".").Reverse().ToList();
+			var subdomains = new List<string>();
+			for (var i = 0; i < domains.Count; i++) {
+				subdomains.Add(String.Join(".", domains.Take(i + 1).Reverse().ToList()));
+			}
+			return subdomains;
+		}
 
 		public Task<IP4Addr> ResolveRecursive(string domain)
 		{
 			return Task<IP4Addr>.Run(() => {
 				string[] domains = domain.Split('.');
+				var subdomains = generateSubdomains(domain);
 				Array.Reverse(domains);
 				IP4Addr res = dnsClient.GetRootServers()[0];
 				String subdomain = null;
@@ -30,40 +42,52 @@ namespace dns_netcore
 				for (var i = 0; i < domains.Length; i++) {
 					// The subdomain name we're querying the server with
 					subdomain = domains[i];
-					// Full path of the subdomain we are resolving. For mff.cuni.cz, it would be:
+					// Full path of the subdomain we are resolving. For mff.cuni.cz, it would be (in order of iteration):
 					// cz -> cuni.cz -> mff.cuni.cz
 					var fullSubdomain = String.Join(".", domains.Take(i + 1).Reverse().ToList());
 				
 					// Directly resolve the domain (slow)
 					var fallbackQuery = this.dnsClient.Resolve(res, subdomain);
 
-					bool isCacheUsed = false;
-					// In the meantime, check if the subdomain's IP is cached
-					if (this.cache.ContainsKey(fullSubdomain)) {
-						IP4Addr cachedIP;
+					// In the meantime, check if any subdomain IP is cached
+					IP4Addr cachedIP;
+					var cacheValidations = new List<(string, Task<string>)>();
+					// Try all subdomains of the current domain
+					foreach (var cachedSubdomain in subdomains.Skip(i)) {
 						if (this.cache.TryGetValue(fullSubdomain, out cachedIP)) {
 							// Possible cache hit, check if the record is still valid
 							var cacheQuery = this.dnsClient.Reverse(cachedIP);
-							// Guaranteed (?) to be faster than the fallbackQuery
-							// TODO: Wait for both Tasks, just in case the fallbackQuery finishes first
-							cacheQuery.Wait();
-							if (cacheQuery.Result == fullSubdomain) {
-								Console.WriteLine($"{domain} -- Cache hit for {fullSubdomain} as {res}");
-								res = cachedIP;
-								isCacheUsed = true;
-							} else {
-								this.cache.TryRemove(fullSubdomain, out _);
-							}
+							cacheValidations.Add((cachedSubdomain, cacheQuery));
 						}
 					}
-					// Cache failed, resolve directly
-					if (!isCacheUsed) {
+					// Wait for the first Reverse Task to finish and then wait a bit for the other Tasks
+					// That way, a stuck Task will not block
+					Task.WaitAny(cacheValidations.Select(i => i.Item2).ToArray(), 550);
+					System.Threading.Thread.Sleep(50);
+
+					var finishedTasks = cacheValidations.FindAll(r => r.Item2.Status == TaskStatus.RanToCompletion).ToList();
+					// Remove failed Reverse checks from the cache
+					foreach (var failedValidation in finishedTasks.FindAll(validation => validation.Item1 != validation.Item2.Result)) {
+						this.cache.TryRemove(failedValidation.Item1, out _);
+					}
+					// Find "longest" subdomain that is validated
+					var validatedSubdomain = finishedTasks.FindAll(validation => validation.Item1 == validation.Item2.Result)
+							.Select(validation => validation.Item1)
+							.OrderBy(x => x.Split(".").Length)
+							.FirstOrDefault();
+
+					// Using cached address
+					if (!string.IsNullOrEmpty(validatedSubdomain) && this.cache.TryGetValue(validatedSubdomain, out res)) {
+						// Calculate how many subdomains we've jumped ahead by using the cached results
+						i = validatedSubdomain.Split(".").Length - 1;
+					} else {
 						fallbackQuery.Wait();
 						res = fallbackQuery.Result;
 						// Console.WriteLine($"Resolved {subdomain} to {res}");
 						this.cache.TryAdd(fullSubdomain, res);
 						// Console.WriteLine($"Caching {fullSubdomain}");
 					}
+
 				}
 				this.cache.TryAdd(domain, res);
 				// Console.WriteLine($"Caching {domain} to {res}");
